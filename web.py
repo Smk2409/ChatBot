c7c8f7e14ae1a9ce1308b60746489a90fd09302c
SELVAMUTHUKUMARAN G <selvamuthukumaran2409@gmail.com>
2026-06-25 14:25:50 +0530

    Add files via upload

---
+++ web.py
@@ -0,0 +1,34 @@
+from flask import Flask,render_template,request
+from openai import OpenAI
+import pypdf
+app = Flask(__name__)
+client = OpenAI(
+    base_url="https://router.huggingface.co/v1",
+    api_key="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"
+)
+@app.route('/',methods=['GET','POST'])
+def home():
+    output=""
+    reply=""
+    if request.method=='POST':
+        user=request.form['user']
+        file = request.files['file']
+        pdf_reader = pypdf.PdfReader(file)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        output=text
+        completion = client.chat.completions.create(
+          model="deepseek-ai/DeepSeek-R1",
+         messages=[
+         {
+             "role": "user",
+             "content":f"document informsation: {output},user query: {user}"
+          }
+        ],
+        )
+        reply = completion.choices[0].message
+    return render_template('index.html',output=reply)
+
+if __name__=="__main__":
+    app.run()
